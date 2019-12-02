0e62e16751ee7c573385b80b0b63afdd8a718f54
Pedro <Oliveira>
2019-12-02 23:35:54 +0000

    alala

--- Assets/VRTK/Examples/ExampleResources/SceneResources/[006 - Locomotion] MovementTypes/Scripts/ElevatorControl.cs
+++ Assets/VRTK/Examples/ExampleResources/SceneResources/[006 - Locomotion] MovementTypes/Scripts/ElevatorControl.cs
@@ -8,7 +8,42 @@
         public VRTK_BaseControllable controllable;
         public GameObject platform;
         public GameObject pivotPoint;
+        public GameObject wheel;
         public float speed;
+        public float value;
+        public float lastValue = 0;
+        public int side = 1;
+
+        private void Update()
+        {
+            if (wheel == null)
+            {
+                if (controllable.gameObject.name == "WheelControl1")
+                {
+                    wheel = GameObject.Find("[VRTK][AUTOGEN][WheelControl1][Controllable][ArtificialBased][RotatorContainer]");
+                    lastValue = wheel.transform.rotation.y;
+                }
+
+                if (controllable.gameObject.name == "WheelControl2")
+                {
+                    wheel = GameObject.Find("[VRTK][AUTOGEN][WheelControl2][Controllable][ArtificialBased][RotatorContainer]");
+                    lastValue = wheel.transform.rotation.y;
+                }
+            }
+
+            if (lastValue < wheel.transform.rotation.y)
+            {
+                Debug.Log(false);
+                side = (-1);
+            }
+            else if (lastValue > wheel.transform.rotation.y)
+            {
+                Debug.Log(true);
+                side = 1;
+            }
+
+            lastValue = wheel.transform.rotation.y;
+        }
 
         protected virtual void OnEnable()
         {
@@ -22,6 +57,7 @@
         {
             if (controllable != null)
             {
+                
                 controllable.ValueChanged -= ValueChanged;
             }
         }
@@ -31,9 +67,9 @@
         {
             if (platform != null)
             {
-
                 platform.transform.RotateAround(pivotPoint.transform.position, Vector3.up, (speed * e.normalizedValue * Time.deltaTime));
-                platform.transform.Translate(Vector3.forward * (e.normalizedValue * Time.deltaTime));
+                platform.transform.Translate(side * Vector3.forward * (e.normalizedValue * Time.deltaTime));
+                
             }
         }
     }
