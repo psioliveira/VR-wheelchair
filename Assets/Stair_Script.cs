3129a25c0f01349cae7a0686688817abfc6f6280
Pedro <pedrooliveira2007@hotmail.com>
2019-12-16 20:00:54 +0000

    reformated game

---
+++ Assets/Stair_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Stair_Script : MonoBehaviour
+{
+    [SerializeField]
+    VRTK_BodyPhysics body;
+    Rigidbody me;
+
+    private void LateStart()
+    {
+        me = GetComponent<Rigidbody>();
+        
+    }
+
+    private void Update()
+    {
+        transform.rotation = me.rotation;
+        body.enableBodyCollisions = false;
+    }
+    private void LateUpdate()
+    {
+        body.enableBodyCollisions = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Rigid")
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddForce(transform.forward * 80);
+        }
+    }
+}
--- Assets/VRTK/Examples/ExampleResources/SceneResources/[006 - Locomotion] MovementTypes/Scripts/ElevatorControl.cs
+++ Assets/VRTK/Examples/ExampleResources/SceneResources/[006 - Locomotion] MovementTypes/Scripts/ElevatorControl.cs
@@ -13,8 +13,39 @@
         public float value;
         public float lastValue = 0;
         public int side = 1;
+        private float oldrot, newrot;
+        private int direction = 0;
+ 
+        private void LateUpdate()
+        {
+            newrot = controllable.GetValue();
+        }
 
-       
+        private void Update()
+        {
+            if(oldrot > newrot)
+            {
+                direction = -1;
+            }
+            if(oldrot < newrot)
+            {
+                direction = 1;
+            }
+            oldrot = newrot;
+            //if (Vector3.Dot(fwd, movement) < 0)
+            //{
+            //    Debug.Log("Back");
+            //    direction = -1;
+            //}
+            //else if (Vector3.Dot(fwd, movement) > 0)
+            //    direction = 1;
+            //else
+            //{
+            //    Debug.Log("Forward");
+            //    direction = 0;
+            //}
+        }
+
 
         protected virtual void OnEnable()
         {
@@ -37,11 +68,36 @@
         [System.Obsolete]
         protected virtual void ValueChanged(object sender, ControllableEventArgs e)
         {
+            Debug.Log(pivotPoint.transform.position);
             if (platform != null)
             {
-                platform.transform.RotateAround(pivotPoint.transform.position, Vector3.up, (speed * e.normalizedValue * Time.deltaTime));
-                platform.transform.Translate(side * Vector3.forward * (e.normalizedValue * Time.deltaTime));
+                if(direction == 1)
+                {
 
+                	if(wheel.name == "Wheel1"){
+                		 platform.transform.RotateAround(pivotPoint.transform.position, Vector3.up, -(speed * e.normalizedValue * Time.deltaTime));
+                    platform.transform.Translate(side * Vector3.forward * (e.normalizedValue * Time.deltaTime));
+                	}
+
+                	if(wheel.name == "Wheel2"){
+                		 platform.transform.RotateAround(pivotPoint.transform.position, Vector3.up, (speed * e.normalizedValue * Time.deltaTime));
+                    platform.transform.Translate(side * Vector3.forward * (e.normalizedValue * Time.deltaTime));
+                	}
+                   
+
+                }
+                else if(direction == -1)
+                {
+					if(wheel.name == "Wheel1"){
+					platform.transform.RotateAround(pivotPoint.transform.position, Vector3.up, -(speed * e.normalizedValue * Time.deltaTime));
+                    platform.transform.Translate(side * Vector3.forward * -(e.normalizedValue * Time.deltaTime));	
+					}
+
+					if(wheel.name == "Wheel2"){
+					platform.transform.RotateAround(pivotPoint.transform.position, Vector3.up, (speed * e.normalizedValue * Time.deltaTime));
+                    platform.transform.Translate(side * Vector3.forward * -(e.normalizedValue * Time.deltaTime));	
+					}                    
+                }
             }
         }
     }
